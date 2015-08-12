1250cd27ae310a04ca1433686108a5e8f9ba1c18
henrygbc <henry.bravo89@gmail.com>
2015-08-11 23:13:58 -0430

    Serializer with Class Based Views using APIView Class

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import include, url
 from blog.api import views
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'apihenrygbc.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^blog/$', views.blog_list),
-    url(r'^blog/(?P<pk>[0-9]+)/$', views.blog_entry_detail),
+    #Functions Views
+    #url(r'^blog/$', views.blog_list),
+    #url(r'^blog/(?P<pk>[0-9]+)/$', views.blog_entry_detail),
+    url(r'^blog/$', views.BlogListView.as_view()),
+    url(r'^blog/(?P<pk>[0-9]+)/$', views.BlogEntryDetail.as_view()),
 ]
--- blog/api/views.py
+++ blog/api/views.py
@@ -1,12 +1,70 @@
-from django.http import HttpResponse
+
+from django.http import HttpResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.decorators import api_view
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from blog.models import Blog
 from blog.api.serializers import BlogSerializer
 
+"""
+SERIALIZERS WITH  CLASS BASED VIEWS USING CLASS APIView
+"""
+
+class BlogListView(APIView):
+    """
+    List all entry blogs or create a new entry
+    """
+
+    def get(self, request, format=None):
+        entrys = Blog.objects.all()
+        serializer = BlogSerializer(entrys, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class BlogEntryDetail(APIView):
+    """
+    Retrieve, update or delete a entry blog
+    """
+    def get_object(self, pk):
+        try:
+            return Blog.objects.get(pk=pk)
+        except Blog.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        entry = self.get_object(pk)
+        serializer = BlogSerializer(entry)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        entry = self.get_object(pk)
+        serializer = BlogSerializer(entry, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, reqquest, pk, format=None):
+        entry = self.get_object(pk)
+        entry.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+"""
+SERIALIZERS WITH FUNCTION BASED VIEWS
+"""
 
 class JSONResponse(HttpResponse):
     """
@@ -19,9 +77,7 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 
-#Function Views
-
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def blog_list(request):
     """
     List all entry blogs or create a new entry
@@ -39,7 +95,7 @@ def blog_list(request):
             return JSONResponse(serializer.data, status=201)
         return JSONResponse(serializer.errors, status=400)
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def blog_entry_detail(request, pk):
 
     """
