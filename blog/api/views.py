b28fb769dffc44975c0e3f65b1ac7ff71fc5818c
henrygbc <henry.bravo89@gmail.com>
2015-08-11 23:34:02 -0430

    Serializers With Class Based Views Using Mixins Class

--- blog/api/views.py
+++ blog/api/views.py
@@ -8,118 +8,49 @@ from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from blog.models import Blog
+from rest_framework import mixins
+from rest_framework import generics
 from blog.api.serializers import BlogSerializer
 
 """
-SERIALIZERS WITH  CLASS BASED VIEWS USING CLASS APIView
+SERIALIZERS WITH  CLASS BASED VIEWS USING MIXINS CLASS
 """
 
-class BlogListView(APIView):
+class BlogListView(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   generics.GenericAPIView):
     """
     List all entry blogs or create a new entry
     """
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
 
-    def get(self, request, format=None):
-        entrys = Blog.objects.all()
-        serializer = BlogSerializer(entrys, many=True)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request, format=None):
-        serializer = BlogSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.crete(request, *args, **kwargs)
 
 
-class BlogEntryDetail(APIView):
+
+class BlogEntryDetail(mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.DestroyModelMixin,
+                      generics.GenericAPIView):
     """
     Retrieve, update or delete a entry blog
     """
-    def get_object(self, pk):
-        try:
-            return Blog.objects.get(pk=pk)
-        except Blog.DoesNotExist:
-            raise Http404
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
 
-    def get(self, request, pk, format=None):
-        entry = self.get_object(pk)
-        serializer = BlogSerializer(entry)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        entry = self.get_object(pk)
-        serializer = BlogSerializer(entry, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def delete(self, reqquest, pk, format=None):
-        entry = self.get_object(pk)
-        entry.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
-
-"""
-SERIALIZERS WITH FUNCTION BASED VIEWS
-"""
-
-class JSONResponse(HttpResponse):
-    """
-    An HttpResponse that renders its content into JSON.
-    """
-
-    def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
-        kwargs['content_type'] = 'application/json'
-        super(JSONResponse, self).__init__(content, **kwargs)
-
-
-@api_view(['GET', 'POST'])
-def blog_list(request):
-    """
-    List all entry blogs or create a new entry
-    """
-    if request.method == 'GET':
-        entrys = Blog.objects.all()
-        serializer = BlogSerializer(entrys, many = True)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = BlogSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data, status=201)
-        return JSONResponse(serializer.errors, status=400)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def blog_entry_detail(request, pk):
-
-    """
-    Retrieve, update or delete a entry blog
-    """
-
-    try:
-        entry = Blog.objects.get(pk=pk)
-    except Blog.DoesNotExist:
-        return HttpResponse(status=404)
-
-
-    if request.method == 'GET':
-        serializer = BlogSerializer(entry)
-        return JSONResponse(serializer.data)
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = BlogSerializer(entry, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        entry.delete()
-        return HttpResponse(status=204)
 
